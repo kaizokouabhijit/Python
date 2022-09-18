fd201688a884c2187a5d1fd9d56c4f55d32fc14d
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2022-09-18 15:12:11 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,9 @@
-str = "My name is anthony"
+list = [ 50, 40, 30, 45, 33]
 
 
-print(str)
+def my_func(arg):
+    return abs(arg -5)
+
+# print([my_func(x) for x in list])
+list.sort(key=my_func)
+print(list)
