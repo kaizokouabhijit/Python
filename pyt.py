a895a929e620b96a4a6d1db7ede9aa761a97b230
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2022-09-20 22:00:37 +0530

    NA

--- pyt.py
+++ pyt.py
@@ -1,19 +1,32 @@
 import unittest
+from urllib import response
 from moto import mock_s3
 import boto3
 
-def func_to_test():
+def listBuckets():
     s3 = boto3.client('s3')
     response = s3.list_buckets()
     return response['ResponseMetadata']['HTTPStatusCode']
         
-# print(func_to_test())
+# print(listBuckets())
+
+bucket_name = 'operative-aws-trainoing-3'
+def createBucket(bucket_name):
+    s3 = boto3.client('s3')
+    bucket = s3.create_bucket(Bucket = bucket_name,CreateBucketConfiguration={'LocationConstraint':'us-west-1'})
+
+    return(bucket['ResponseMetadata']['HTTPStatusCode'])
+
+# createBucket(bucket_name)
+
+
 
 
 
 class MyTest(unittest.TestCase):
     mock_s3 = mock_s3()
     bucket_name = 'operative-aws-trainoing-3'
+    content = 200
     def setUp(self):
         self.mock_s3.start()
 
@@ -24,19 +37,29 @@ class MyTest(unittest.TestCase):
     def tearDown(self):
         self.mock_s3.stop()
 
-    def test(self):
+    def test_listbucket(self):
         #These are the list of buckets in My AWS S3 right now
-        content = 200
+        
         
 
         
-        func_to_test()
+        listBuckets()
 
         
         s3 = boto3.client('s3')
         
         Bucket_list = s3.list_buckets()   # Bucket_list should be a list, but gives some weird value 
-        self.assertEqual(Bucket_list['ResponseMetadata']['HTTPStatusCode'], content)
+        self.assertEqual(Bucket_list['ResponseMetadata']['HTTPStatusCode'], self.content)
+    
+    def test_createbucket(self):
+
+        createBucket(self.bucket_name)
+
+        s3 = boto3.client('s3')
+        testbucket = "Testbucket"
+        response = s3.create_bucket(Bucket = testbucket, CreateBucketConfiguration={'LocationConstraint':'us-west-1'})
+        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], self.content)
+        
 
         
 
